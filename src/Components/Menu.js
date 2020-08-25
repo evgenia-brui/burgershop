c53f141ea3f9264297485134cda623f38e0f42d4
Evgenia Brui <j-lp@mail.ru>
2020-08-25 14:38:18 +0300

    Добавление и выведение списка меню и банера

---
+++ src/Components/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import dbMenu from './DBMenu';
+import { ListItem } from './ListItem';
+import bannerImg from '../image/banner.png';
+
+const Banner = styled.div`
+    hight: 100%;
+`;
+
+const BannerImg = styled.img`
+    width: 100%;
+    hight: 210px;
+`;
+
+const MenuStyled = styled.main`
+    background-color: #ccc;
+    margin-top: 80px;
+`;
+
+const SectionMenu = styled.section`
+    padding: 30px;
+`;
+
+export const Menu = () => (
+    <MenuStyled>
+        <Banner>
+            <BannerImg src={bannerImg} alt='banner'/>
+        </Banner>
+        <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger}/>
+        </SectionMenu>
+        <SectionMenu>
+            <h2>Закуски / напитки</h2>
+            <ListItem itemList={dbMenu.other}/>
+        </SectionMenu>
+    </MenuStyled>
+
+);
